e139facea71b13864676321e8b7373e2442daedc
Gustavo Eraso <gustavoeraso1988@gmail.com>
2023-06-03 13:27:17 -0300

    styles     - main container mas width     - main background img     - ToDoAdd animated white shadow

--- src/components/ToDoAdd/index.jsx
+++ src/components/ToDoAdd/index.jsx
@@ -1,8 +1,8 @@
-import '../styles/ToDoAdd.css'
+import './styles.css'
 
-import { ReactComponent as AddIcon } from '../assets/icons/add_FILL0_wght700_GRAD200_opsz48.svg'
+import { ReactComponent as AddIcon } from './add_FILL0_wght700_GRAD200_opsz48.svg'
 import { useContext, useEffect, useState } from 'react'
-import { ToDoContext } from './ToDoContext'
+import { ToDoContext } from '../ToDoContext'
 
 
 
